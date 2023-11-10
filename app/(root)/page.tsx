a2a5b20744ac9598620f82b009ea1fbae9889e43
Nguyễn Văn Tài <nvtai4502@gmail.com>
2023-11-10 14:30:31 +0700

    Dark Theme

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,12 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
   <div>
-    Signin/Signup with Clerk
+    Dark Theme
     <UserButton afterSignOutUrl="/" />
+    <ModeToggle />
   </div>
   )
 }
